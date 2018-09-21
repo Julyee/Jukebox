5735696f56447cdc9949ad567f4210db5b9db1b9
Dario Segura <dario.seco@gmail.com>
2018-09-21 16:45:57 -0400

    [API] Token encoder uses command line arguments.

--- keys/music-token-encoder.js
+++ keys/music-token-encoder.js
@@ -4,9 +4,18 @@ const fs = require('fs');
 const path = require('path');
 const jwt = require('jsonwebtoken');
 
-const privateKey = fs.readFileSync(path.resolve(__dirname, './AuthKey_B5398RR63L.p8')).toString();
-const teamId     = "52PP2L8QS9";
-const keyId      = "B5398RR63L";
+if (process.argv.length !== 6) {
+    console.error('ERROR: Incorrect number of arguments.');
+    console.warn('USAGE: music-token-encoder [PRIVATE_KEY_FILE] [TEAM_ID] [KEY_ID] [OUTPUT_FILE]')
+    return;
+}
+
+const keyFilePath = path.resolve(process.cwd(), process.argv[2]);
+const teamId = process.argv[3];
+const keyId = process.argv[4];
+const outputFile = path.resolve(process.cwd(), process.argv[5]);
+
+const privateKey = fs.readFileSync(keyFilePath).toString();
 
 const jwtToken = jwt.sign({}, privateKey, {
     algorithm: "ES256",
@@ -18,7 +27,7 @@ const jwtToken = jwt.sign({}, privateKey, {
     }
 });
 
-fs.writeFileSync(path.resolve(__dirname, "../www/devtoken.jwt"), jwtToken);
+fs.writeFileSync(outputFile, jwtToken);
 console.log('-----BEGIN DEV TOKEN-----');
 console.log(jwtToken);
 console.log('-----END DEV TOKEN-----');
