7678b895681a178cbae9a584cc7718bba2c97b3b
Dario Segura <dario.seco@gmail.com>
2018-09-20 23:07:10 -0400

    [GUI] Uses font awesome for icons

--- src/frontend/header/Header.js
+++ src/frontend/header/Header.js
@@ -3,9 +3,10 @@ import { EventCenter } from '../../core/EventCenter';
 import { IBindable } from '../../core/IBindable';
 import { SearchBar } from './SearchBar';
 import { Events, Buttons } from '../Events';
+import { svgPathData as menuPathData } from '@fortawesome/free-solid-svg-icons/faBars';
 import m from 'mithril';
 
-const iconMenuSVG = '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>';
+const iconMenuSVG = `<svg width="24" height="24" viewBox="-40 -40 600 600"><path d="${menuPathData}"/></svg>`;
 const toolbarButton = svg => m(IconButton, {
     icon: { svg },
     events: {
--- src/frontend/header/SearchBar.js
+++ src/frontend/header/SearchBar.js
@@ -1,5 +1,7 @@
 import { IBindable } from '../../core/IBindable';
 import { Search, IconButton } from 'polythene-mithril';
+import { svgPathData as searchPathData } from '@fortawesome/free-solid-svg-icons/faSearch';
+import { svgPathData as clearPathData } from '@fortawesome/free-solid-svg-icons/faTimesCircle';
 import stream from 'mithril/stream';
 import m from 'mithril';
 
@@ -17,9 +19,8 @@ IconButtonCSS.addStyle('.jukebox-search-icon', {
     padding: 0,
 });
 
-const iconSearchSVG = '<svg width="24" height="24" viewBox="-4 -4 30 30"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>';
-// const iconBackSVG = '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>';
-const iconClearSVG = '<svg width="24" height="24" viewBox="-4 -4 32 32"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>';
+const iconSearchSVG = `<svg width="24" height="24" viewBox="-150 -150 800 800"><path d="${searchPathData}"/></svg>`;
+const iconClearSVG = `<svg width="24" height="24" viewBox="-150 -150 800 800"><path d="${clearPathData}"/></svg>`;
 
 const iconSearch = m.trust(iconSearchSVG);
 const iconClear = m.trust(iconClearSVG);
