dd2df9a5931208c5d9d4d468f0ba43c0c8d30f71
Dario Segura <dario.seco@gmail.com>
2018-09-21 22:30:17 -0400

    [MK] Initial MusicKit implementation

---
+++ src/service/AppleService.js
@@ -0,0 +1,44 @@
+/* global MusicKit */
+
+import { Service } from './Service';
+import m from 'mithril';
+
+export class AppleService extends Service {
+    constructor() {
+        super();
+        this.mAPI = null;
+    }
+
+    get authorized() {
+        return this.mAPI.isAuthorized;
+    }
+
+    async init(devTokenPath, appName, build) {
+        const devToken = await m.request({
+            method: 'GET',
+            url: devTokenPath,
+            deserialize: value => value,
+        });
+        this.mAPI = MusicKit.configure({
+            developerToken: devToken,
+            app: {
+                name: appName,
+                build: build,
+            },
+        });
+    }
+
+    async authorize() {
+        if (!this.authorized) {
+            await this.mAPI.authorize();
+        }
+        return this.mAPI.musicUserToken;
+    }
+
+    async unauthorize() {
+        if (this.authorized) {
+            return await this.mAPI.unauthorize();
+        }
+        return true;
+    }
+}
