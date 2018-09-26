85297f16117decb1ba77055242fccd14270fb200
Dario Segura <dario.seco@gmail.com>
2018-09-26 00:03:13 -0400

    [EVENTS] Added player events.

--- src/frontend/Events.js
+++ src/frontend/Events.js
@@ -1,7 +1,20 @@
 export const Events = {
     BUTTON_PRESS: 'GUI::ButtonPress',
+
+    SONG_LOADING: 'PLR::SongLoading',
+    SONG_PLAY: 'PLR::SongPlay',
+    SONG_PAUSE: 'PLR::SongPause',
+    SONG_STOP: 'PLR::SongStop',
+    SONG_END: 'PLR::SongEnd',
+    SONG_COMPLETE: 'PLR::SongComplete',
+
+    PLAYER_TIME_CHANGE: 'PLR::TimeChange',
 };
 
 export const Buttons = {
-    DRAWER_BUTTON: 'GUI::Drawer',
+    DRAWER_BUTTON: 'BTN::MENU::Drawer',
+
+    PLAY_SONG_BUTTON: 'BTN::SONG::Play',
+    PAUSE_SONG_BUTTON: 'BTN::SONG::Pause',
+    STOP_SONG_BUTTON: 'BTN::SONG::Stop',
 };
