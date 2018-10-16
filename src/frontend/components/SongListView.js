6c784e3f8b2c6cf61a2c174b7bdc22ebaaf1bcd6
Dario Segura <dario.seco@gmail.com>
2018-10-16 00:00:59 -0400

    [ALBUM] Initial implementation of the album view.

---
+++ src/frontend/components/SongListView.js
@@ -0,0 +1,65 @@
+import m from 'mithril';
+import {Button, Dialog, List} from 'polythene-mithril';
+import {SongItem} from './SongItem';
+import {EventCenter} from '../../core/EventCenter';
+import {Events} from '../Events';
+import {ButtonCSS} from 'polythene-css';
+
+ButtonCSS.addStyle('.song-list-view-button', {
+    'label_padding_v': 3,
+    'border_radius': 10,
+    'outer_padding_v': 0,
+    // text_transform: 'none',
+    'color_light_background': 'rgb(21,150,199)',
+});
+
+export class SongListView {
+    view(vnode) {
+        const options = vnode.attrs;
+        const buttons = options.hasOwnProperty('buttons') ? options.buttons : [];
+        const date = new Date(options.date);
+        const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        return m('.song-list-view-container', [
+            m('.song-list-view-header', [
+                m('.song-list-view-artwork', {
+                    style: {
+                        'background-image': `url(${options.artworkURL})`,
+                    },
+                }),
+                m('.song-list-view-info-container', [
+                    options.isExplicit ? m('.song-list-view-explicit', '🅴') : null,
+                    m('.song-list-view-name', options.title),
+                    m('.song-list-view-artist', options.subtitle),
+                    m('.song-list-view-date', date.toLocaleDateString("en-US", dateOptions)),
+                    m('.song-list-view-genre', options.genres),
+                    m('.song-list-view-buttons', buttons.map(button => m(Button, {
+                        label: button.label,
+                        className: 'song-list-view-button',
+                        textStyle: {
+                            'font-size': '9px',
+                            'color': '#f9f9ff',
+                        },
+                        events: {
+                            onclick: () => {
+                                Dialog.hide().then(() => EventCenter.emit(Events.BUTTON_PRESS, button.event, button.eventData));
+                            },
+                        },
+                    }))),
+                ]),
+            ]),
+            options.description ? m('.song-list-view-description', m.trust(options.description)) : null,
+            m('.song-list-view-songs-container', m(List, {
+                border: true,
+                indentedBorder: false,
+                compact: false,
+                padding: 'none',
+                tiles: options.songs.map(song => m(SongItem, {
+                    song: song,
+                    size: 52,
+                    displayThumbnail: options.displayThumbnail,
+                    moreDialogOptions: options.moreDialogOptions,
+                })),
+            })),
+        ]);
+    }
+}
--- src/frontend/routes/Album.js
+++ src/frontend/routes/Album.js
@@ -1,6 +1,9 @@
 import {Layout} from '../Layout';
 import {Service} from '../../service/Service';
 import m from 'mithril';
+import {IOSSpinner} from 'polythene-mithril';
+import {SongListView} from '../components/SongListView';
+import {Buttons} from '../Events';
 
 export class Album extends Layout {
     constructor() {
@@ -21,7 +24,47 @@ export class Album extends Layout {
     }
 
     content() {
+        if (!this.mAlbum && !this.mAlbumID && !this.mSongID) {
+            return m('.album-view-screen-center', m('.album-view-error', 'ERROR: Could not load album, please try again.'));
+        } else if (!this.mAlbum && (this.mAlbumID || this.mSongID)) {
+            return m('.album-view-screen-center',
+                m('.album-view-loading', [
+                    m(IOSSpinner, {
+                        permanent: true,
+                        show: true,
+                        raised: false,
+                        singleColor: true,
+                        className: 'album-view-loading-icon',
+                    }),
+                    m('.album-view-loading-text', 'Loading...'),
+                ]),
+            );
+        }
 
+        const album = this.mAlbum;
+        const artworkURL = album.formatArtworkURL(400, 400);
+
+        return m(SongListView, {
+            artworkURL: artworkURL,
+            title: album.name,
+            subtitle: album.artist,
+            isExplicit: album.isExplicit,
+            date: album.releaseDate,
+            genres: album.genres.join(', '),
+            description: album.longDescription,
+            songs: album.songs,
+            displayThumbnail: false,
+            moreDialogOptions: {
+                showAlbumButton: false,
+            },
+            buttons: [
+                {
+                    label: 'Artist',
+                    event: Buttons.ARTIST_OPEN_VIEW,
+                    eventData: null, // TODO
+                },
+            ],
+        });
     }
 
     _updateAlbum() {
@@ -33,10 +76,9 @@ export class Album extends Layout {
             this.mAlbumID = albumID || null;
             this.mSongID = songID || null;
             this.mServiceName = serviceName || null;
-
-            if (albumID || songID) {
+            const service = Service.activeService(serviceName ? serviceName : null);
+            if (service && (albumID || songID)) {
                 this.mAlbum = null;
-                const service = Service.activeService(serviceName ? serviceName : null);
                 let p;
                 if (albumID) {
                     p = service.getAlbumInfo(albumID);
