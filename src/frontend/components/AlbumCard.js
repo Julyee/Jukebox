565526aa66a41d0c18ca85c61c50ba67c0e815b8
Dario Segura <dario.seco@gmail.com>
2018-10-15 23:56:47 -0400

    [ALBUM] Fixes artwork download size

--- src/frontend/components/AlbumCard.js
+++ src/frontend/components/AlbumCard.js
@@ -8,8 +8,7 @@ export class AlbumCard extends LargeThumbnailCard {
     }
 
     _getContent(album, size = 180) {
-        const artworkSize = (size * window.devicePixelRatio);
-        const artworkURL = album.formatArtworkURL(artworkSize, artworkSize);
+        const artworkURL = album.formatArtworkURL(size, size);
         return super._getContent(album.name, album.artist, artworkURL, album.isExplicit, () => {
             EventCenter.emit(GeneralEvents.BUTTON_PRESS, Buttons.ALBUM_OPEN_VIEW, album);
         });
