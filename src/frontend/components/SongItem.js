585aa196a9f0b2e2b837f8164a2a117c4bf29fde
Dario Segura <dario.seco@gmail.com>
2018-10-15 23:57:48 -0400

    [SONG] Song item forwards header buttons configuration

--- src/frontend/components/SongItem.js
+++ src/frontend/components/SongItem.js
@@ -3,15 +3,17 @@ import {ListTile} from 'polythene-mithril';
 import {EventCenter} from '../../core/EventCenter';
 import {Buttons, Events} from '../Events';
 import * as moreSVG from '@fortawesome/free-solid-svg-icons/faEllipsisH';
+import * as playSVG from '@fortawesome/free-solid-svg-icons/faPlayCircle';
 
 const iconMore = m.trust(`<svg width="15" height="15" viewBox="0 0 ${moreSVG.width} ${moreSVG.height}"><path d="${moreSVG.svgPathData}"/></svg>`);
+const iconPlay = m.trust(`<svg width="38" height="38" viewBox="0 0 ${playSVG.width} ${playSVG.height}"><path d="${playSVG.svgPathData}"/></svg>`);
 
 export class SongItem {
     view(vnode) {
-        return this._getContent(vnode.attrs.song, vnode.attrs.size);
-    }
-
-    _getContent(song, size = 52) {
+        const song = vnode.attrs.song;
+        const size = vnode.attrs.size || 52;
+        const moreDialogOptions = vnode.attrs.moreDialogOptions || {};
+        const displayThumbnail = vnode.attrs.hasOwnProperty('displayThumbnail') ? vnode.attrs.displayThumbnail : true;
         const artworkURL = song.formatArtworkURL(size, size);
         return m(ListTile, {
             subContent: m('.song-item-details', [
@@ -27,13 +29,13 @@ export class SongItem {
             events: {
                 onclick: () => EventCenter.emit(Events.BUTTON_PRESS, Buttons.SONG_PLAY_NOW, song),
             },
-            front: m('.song-item-song-icon', {
+            front: displayThumbnail ? m('.song-item-song-icon', {
                 style: {
                     'background-image': `url(${artworkURL})`,
                 },
-            }),
+            }) : m('.song-item-play-icon', iconPlay),
             after: m('.song-item-more-container', {
-                onclick: () => EventCenter.emit(Events.BUTTON_PRESS, Buttons.SONG_MORE, song),
+                onclick: () => EventCenter.emit(Events.BUTTON_PRESS, Buttons.SONG_MORE, song, moreDialogOptions),
             }, iconMore),
         });
     }
