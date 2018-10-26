951239bf6aaad875be1044b9d77235c4520555ac
Dario Segura <dario.seco@gmail.com>
2018-10-25 23:34:31 -0400

    [GUI] Song list view supports more button + uses media item

--- src/frontend/components/SongListView.js
+++ src/frontend/components/SongListView.js
@@ -1,9 +1,10 @@
 import m from 'mithril';
-import {Button, Dialog, List} from 'polythene-mithril';
+import {Button, IconButton, Dialog, List, Icon} from 'polythene-mithril';
 import {SongItem} from './SongItem';
 import {EventCenter} from '../../core/EventCenter';
-import {Events} from '../Events';
+import {Buttons, Events} from '../Events';
 import {ButtonCSS} from 'polythene-css';
+import * as moreSVG from '../../../node_modules/@fortawesome/free-solid-svg-icons/faEllipsisH';
 
 ButtonCSS.addStyle('.song-list-view-button', {
     'label_padding_v': 3,
@@ -13,52 +14,71 @@ ButtonCSS.addStyle('.song-list-view-button', {
     'color_light_background': 'rgb(21,150,199)',
 });
 
+const iconMore = m.trust(`<svg width="15" height="15" viewBox="0 0 ${moreSVG.width} ${moreSVG.height}"><path d="${moreSVG.svgPathData}"/></svg>`);
+
 export class SongListView {
     view(vnode) {
         const options = vnode.attrs;
+        const mediaItem = options.mediaItem;
         const buttons = options.hasOwnProperty('buttons') ? options.buttons : [];
-        const date = options.date ? new Date(options.date) : null;
+        const date = mediaItem.releaseDate ? new Date(mediaItem.releaseDate) : null;
         const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        const headerButtons = buttons.map(button => m(Button, {
+            label: button.label,
+            className: 'song-list-view-button',
+            textStyle: {
+                'font-size': '9px',
+                'color': '#f9f9ff',
+            },
+            events: {
+                onclick: () => {
+                    Dialog.hide().then(() => EventCenter.emit(Events.BUTTON_PRESS, button.event, button.eventData));
+                },
+            },
+        }));
+
+        if (options.moreDialogOptions) {
+            headerButtons.push(m(IconButton, {
+                inactive: false,
+                events: {
+                    onclick: () => EventCenter.emit(Events.BUTTON_PRESS, Buttons.SONG_MORE, mediaItem, options.moreDialogOptions),
+                },
+            },
+            m(Icon, {
+                svg: iconMore,
+                size: 'small',
+                className: 'song-list-view-more-container',
+            })));
+        }
+
         return m('.song-list-view-container', [
             m('.song-list-view-header', [
-                m('.song-list-view-artwork', {
+                options.artworkURL ? m('.song-list-view-artwork', {
                     style: {
                         'background-image': `url(${options.artworkURL})`,
                     },
-                }),
+                }) : null,
                 m('.song-list-view-info-container', [
-                    options.isExplicit ? m('.song-list-view-explicit', '🅴') : null,
-                    options.title ? m('.song-list-view-title', options.title) : null,
-                    options.subtitle ? m('.song-list-view-subtitle', options.subtitle) : null,
+                    mediaItem.isExplicit ? m('.song-list-view-explicit', '🅴') : null,
+                    mediaItem.title ? m('.song-list-view-title', mediaItem.title) : null,
+                    mediaItem.subtitle ? m('.song-list-view-subtitle', mediaItem.subtitle) : null,
                     date ? m('.song-list-view-date', date.toLocaleDateString('en-US', dateOptions)) : null,
-                    options.genres ? m('.song-list-view-genre', options.genres) : null,
-                    options.duration ? m('.song-list-view-duration', options.duration) : null,
-                    m('.song-list-view-buttons', buttons.map(button => m(Button, {
-                        label: button.label,
-                        className: 'song-list-view-button',
-                        textStyle: {
-                            'font-size': '9px',
-                            'color': '#f9f9ff',
-                        },
-                        events: {
-                            onclick: () => {
-                                Dialog.hide().then(() => EventCenter.emit(Events.BUTTON_PRESS, button.event, button.eventData));
-                            },
-                        },
-                    }))),
+                        mediaItem.genres ? m('.song-list-view-genre', mediaItem.genres.join(', ')) : null,
+                    mediaItem.formattedDuration ? m('.song-list-view-duration', mediaItem.formattedDuration) : null,
+                    m('.song-list-view-buttons', headerButtons),
                 ]),
             ]),
-            options.description ? m('.song-list-view-description', m.trust(options.description)) : null,
-            options.songs ? m('.song-list-view-songs-container', m(List, {
+            mediaItem.longDescription ? m('.song-list-view-description', m.trust(mediaItem.longDescription)) : null,
+            mediaItem.songs ? m('.song-list-view-songs-container', m(List, {
                 border: true,
                 indentedBorder: false,
                 compact: false,
                 padding: 'none',
-                tiles: options.songs.map(song => m(SongItem, {
+                tiles: mediaItem.songs.map(song => m(SongItem, {
                     song: song,
                     size: 52,
-                    displayThumbnail: options.displayThumbnail,
-                    moreDialogOptions: options.moreDialogOptions,
+                    displayThumbnail: options.songDisplayThumbnail,
+                    moreDialogOptions: options.songMoreDialogOptions,
                 })),
             })) : null,
         ]);
