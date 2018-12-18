f2fe6699f179978d597f378aff363afa12011b79
Dario Segura <dario.seco@gmail.com>
2018-12-17 21:40:35 -0500

    [QR] Displays the camera at the correct size on iOS.

--- src/frontend/components/QRScanner.js
+++ src/frontend/components/QRScanner.js
@@ -5,43 +5,32 @@ QRScannerImp.WORKER_PATH = QRWorkerURL;
 
 export class QRScanner {
     oninit (vnode) {
-        vnode.state.videoSize = vnode.attrs.videoSize ? vnode.attrs.videoSize : 250;
-        vnode.state.videoElement = m('video', {
-            muted: ' ',
-            autoplay: ' ',
-            playsinline: ' ',
-        });
+        vnode.state.targetVideoSize = vnode.attrs.videoSize ? vnode.attrs.videoSize : 250;
+        vnode.state.videoSize = { w: 0, h: 0 };
         vnode.state.offset = { x: 0, y: 0 };
         vnode.state.qrScanner = null;
+
+        vnode.state.video = document.createElement('video');
+        vnode.state.video.setAttribute('autoplay', '');
+        vnode.state.video.setAttribute('muted', '');
+        vnode.state.video.setAttribute('playsinline', '');
+        vnode.state.video.width = 0;
+        vnode.state.video.height = 0;
     }
 
     oncreate(vnode) {
-        vnode.state.qrScanner = new QRScannerImp(vnode.state.videoElement.dom, result => {
+        const container = document.getElementById('qrScannerVideoElement');
+        vnode.state.video.addEventListener('loadedmetadata', () => {
+            this._calculateOffsets(vnode, vnode.state.video);
+            vnode.state.video.width = vnode.state.videoSize.w;
+            vnode.state.video.height = vnode.state.videoSize.h;
+            container.appendChild(vnode.state.video);
+        });
+
+        vnode.state.qrScanner = new QRScannerImp(vnode.state.video, result => {
             if (vnode.attrs.onCode) {
                 vnode.attrs.onCode(result);
             }
-        });
-
-        vnode.state.videoElement.dom.addEventListener('loadedmetadata', () => {
-            const videoWidth = vnode.state.videoElement.dom.videoWidth;
-            const videoHeight = vnode.state.videoElement.dom.videoHeight;
-            let width;
-            let height;
-
-            if (videoWidth < videoHeight) {
-                vnode.state.videoElement.dom.width = vnode.state.videoSize;
-                width = vnode.state.videoSize;
-                height = (videoHeight / videoWidth) * vnode.state.videoSize;
-            } else {
-                vnode.state.videoElement.dom.height = vnode.state.videoSize;
-                width = (videoWidth / videoHeight) * vnode.state.videoSize;
-                height = vnode.state.videoSize;
-            }
-
-            vnode.state.offset.x = Math.round((width - vnode.state.videoSize) * 0.5);
-            vnode.state.offset.y = Math.round((height - vnode.state.videoSize) * 0.5);
-
-            m.redraw();
         });
 
         vnode.state.qrScanner.start();
@@ -51,14 +40,36 @@ export class QRScanner {
         if (vnode.state.qrScanner) {
             vnode.state.qrScanner.stop();
         }
-        vnode.state.videoElement = null;
     }
 
     view(vnode) {
         return m('.qr-scanner-camera-container',
             m('.qr-scanner-camera', m('div', {
                 style: `margin-left: -${vnode.state.offset.x}px; margin-top: -${vnode.state.offset.y}px;`,
-            }, vnode.state.videoElement)),
+            }, m('div', { id: 'qrScannerVideoElement' }))),
         );
+    }
+
+    _calculateOffsets(vnode, element) {
+        const videoWidth = element.videoWidth;
+        const videoHeight = element.videoHeight;
+        let width;
+        let height;
+
+        if (videoWidth < videoHeight) {
+            width = vnode.state.targetVideoSize;
+            height = (videoHeight / videoWidth) * vnode.state.targetVideoSize;
+        } else {
+            vnode.state.videoHeight = vnode.state.targetVideoSize;
+            width = (videoWidth / videoHeight) * vnode.state.targetVideoSize;
+            height = vnode.state.targetVideoSize;
+        }
+
+        vnode.state.videoSize.w = Math.round(width);
+        vnode.state.videoSize.h = Math.round(height);
+        vnode.state.offset.x = Math.round((width - vnode.state.targetVideoSize) * 0.5);
+        vnode.state.offset.y = Math.round((height - vnode.state.targetVideoSize) * 0.5);
+
+        m.redraw();
     }
 }
